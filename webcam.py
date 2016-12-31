9244cdd07f6bc6b8839ea56fd617d625da75832a
Daniel Filan <df@danielfilan.com>
2016-12-31 14:46:47 -0800

    it works now!

--- webcam.py
+++ webcam.py
@@ -1,1 +1,15 @@
-from flask import Flask
+from flask import Flask, send_file
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def webcam():
+    randint = random.randint(0,2)
+    if randint == 0:
+        filename = 'gifs/bird_scream.gif'
+    elif randint == 1:
+        filename = 'gifs/meerkat.gif'
+    else:
+        filename = 'gifs/giraffe.gif'
+    return send_file(filename, mimetype='image/gif')
